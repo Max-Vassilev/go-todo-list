fc1928c8092930d472ce5b41adc6ef318f6a57be
Maxim Vassilev <maximvassilev01@gmail.com>
2025-06-11 17:28:41 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var tasks = []string{"Buy groceries", "Complete Go tutorial", "Call Alex"}
+
+func main() {
+	http.HandleFunc("/tasks", showTasks)
+	http.HandleFunc("/add-task", addTask)
+	http.ListenAndServe(":8080", nil)
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	if len(tasks) == 0 {
+		fmt.Fprintln(writer, "There are no tasks.")
+		return
+	}
+
+	fmt.Fprintln(writer, "My tasks:")
+	fmt.Fprintln(writer)
+
+	for _, task := range tasks {
+		fmt.Fprintln(writer, task)
+	}
+}
+
+func addTask(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		var task = request.FormValue("task")
+		tasks = append(tasks, task)
+		fmt.Fprintln(writer, "Task added.")
+	} else {
+		fmt.Fprintln(writer, "Use POST method to add a task.")
+	}
+}
